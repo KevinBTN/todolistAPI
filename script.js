6f6aa177d719bd1f1cbc2c9244e11d1c5cf9f600
Kevin BRANTONE <kbrantone@gmail.com>
2021-12-03 18:12:43 +0100

    ca marche \o/

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let token;
-let dates = []
+const tasks = [];
 const TOKEN_KEY = "token";
 const myHeaders = {'accept': 'application/json',
 'Content-Type': 'application/json'};
@@ -36,8 +36,8 @@ const createHtml = (task) =>{
     </li>
     <li class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">
     <div class="d-flex flex-row justify-content-end mb-1">
-    <a href="#!" class="text-info" data-mdb-toggle="tooltip" title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a>
-    <a href="#!" class="text-danger" data-mdb-toggle="tooltip" title="Delete todo"><i class="fas fa-trash-alt"></i></a>
+    <a href="${tasks[0].indexOf(task)}" class="editTaskBtn text-info" data-mdb-toggle="tooltip" title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a>
+    <a href="#!" class=" text-danger" data-mdb-toggle="tooltip" title="Delete todo"><i class="fas fa-trash-alt"></i></a>
     </div>
     <div class="text-end text-muted">
     <a href="#!" class="text-muted" data-mdb-toggle="tooltip" title="Created date">
@@ -46,8 +46,21 @@ const createHtml = (task) =>{
     </li>
     </ul>
     
-    <div class="card-body">
-    <form action="" class="editForm">
+    <div id="edit${tasks[0].indexOf(task)}" class="card-body">
+    
+    </div>
+    `
+    listContainer.innerHTML += tasksHtml;
+
+
+
+    
+}
+
+const editHTML = (task) => {
+    
+    editTaskHtml = ``;
+    editTaskHtml = `<form action="" class="editForm">
     <div class="d-flex flex-row align-items-center">
     <input type="text" class="form-control form-control-lg" value="${task.Title}">  
     </div>
@@ -65,33 +78,33 @@ const createHtml = (task) =>{
     <input  type="hidden" value="${task.id}">
     <button type="submit"  class="btn mt-3 btn-primary">Valider la modification</button>
     </form>
-    </div>
     `
-    
-    listContainer.innerHTML += tasksHtml;
-    
+    document.getElementById(`edit${tasks[0].indexOf(task)}`).innerHTML = editTaskHtml;
     document.querySelectorAll(".editForm").forEach(form =>{
         form.addEventListener("submit", (e)=>{
             e.preventDefault();
             editTask(form);
+            getTask();
         });
     })
-    
-    
+    $(".datepick").each(function(){
+        $(this).datepicker({ dateFormat: 'yy-mm-dd' }).val();
+        $(this).datepicker("setDate",  `${tasks[0][tasks[0].indexOf(task)].date.split("T")[0]}` ); 
+    })
 }
 
 const editTask = (form)=>{
     fetch(`https://to-do-afpa.forticas.com/public/api/tasks/` + form[4].value, {
-        method: "PUT",
-        headers: {'accept': 'application/json',
-        'Content-Type': 'application/json', 'Authorization' : `Bearer ${localStorage.getItem(TOKEN_KEY)}`},
-        body: JSON.stringify({
-            "Title": `${form[0].value}`,
-            "description": `${form[1].value}`,
-            "date": `${form[3].value}`,
-            "periority": `${form[2].value}`
-        })
+    method: "PUT",
+    headers: {'accept': 'application/json',
+    'Content-Type': 'application/json', 'Authorization' : `Bearer ${localStorage.getItem(TOKEN_KEY)}`},
+    body: JSON.stringify({
+        "Title": `${form[0].value}`,
+        "description": `${form[1].value}`,
+        "date": `${form[3].value}`,
+        "periority": `${form[2].value}`
     })
+})
 }
 
 const getTask = async () =>{
@@ -100,36 +113,29 @@ const getTask = async () =>{
     headers:  {'accept': 'application/json',
     'Content-Type': 'application/json', 'Authorization' : `Bearer ${localStorage.getItem(TOKEN_KEY)}`},
 })
-let tasks = await res.json();
-tasks.forEach(task => {
-    createHtml(task);
-    storeDates(task);
-});
+tasks.push(await res.json());
 console.log(tasks);
-$(".datepick").each(function(){
-    $(this).datepicker({ dateFormat: 'yy-mm-dd' }).val();
-    $(this).datepicker("setDate",  `${tasks[0].date}` );
+if(res.status == 401){
+    localStorage.removeItem('token');
+    getToken;
+}
+tasks[0].forEach(task =>{
+    createHtml(task);
+    console.log(task)
 })
-datePickerSetDate();
-}
-
-const storeDates = (task) =>{
-    dates.push(task.date.split("T")[0]);
-    console.log(dates);
-}
-
-const datePickerSetDate = ()=>{
-    for(let i = 0; i < dates.length; i++){
-        let datepicker = $(".datepick"); 
-        datepicker[0].datepicker("setDate",  `${dates[i]}` );    
-    }
-    
+document.querySelectorAll(".editTaskBtn").forEach(btn =>{
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        editHTML(tasks[0][btn.getAttribute("href")]);
+    })
+})
 }
 
 const newTask = async (title, description, date, priority) =>{
     fetch(`https://to-do-afpa.forticas.com/public/api/tasks`,{
     method: "POST",
-    headers: myHeaders,
+    headers: {'accept': 'application/json',
+    'Content-Type': 'application/json', 'Authorization' : `Bearer ${localStorage.getItem(TOKEN_KEY)}`},
     body: JSON.stringify({
         "Title": `${title}`,
         "description": `${description}`,
@@ -146,6 +152,7 @@ document.getElementById("addTask").addEventListener('submit', (e)=>{
     const datepicker = document.getElementById("datepicker");
     const priority = document.getElementById("priorite");
     newTask(titre.value, description.value, datepicker.value, priority.value);  
+    alert("hey")
 })
 document.addEventListener("DOMContentLoaded", ()=>{
     const application = document.getElementById("application");
@@ -165,8 +172,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
             })
         }
         else{
-            application.classList.remove("d-none");
             getTask();
+            application.classList.remove("d-none");
         }
-        
+        $(".datepick").each(function(){
+            $(this).datepicker({ dateFormat: 'yy-mm-dd' }).val();
+        })
     })
